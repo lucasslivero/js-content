fbcd98f7cd51ccd93f8c6c64acb728601fcbe14b
Lucas Silvestre Livero-LSIL <lsil@motorola.com>
2022-03-23 09:22:41 -0400

    [Backend] Adding Cors and refactory middlewares

--- expressapi/src/index.js
+++ expressapi/src/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
+const cors = require('./middlewares/cors');
+const errorHandler = require('./middlewares/errorHandler');
 require('express-async-errors');
 
 const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
-app.use((request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3002, () => console.log('Server started at http://localhost:3002'));
